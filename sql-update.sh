11f2cc71588d67738247cfe8ab422d239dea9b80
John W Smith <johns@knowclassic.com>
2013-11-18 09:52:58 -0500

    Cleanup
    1. Added --opt and removed redundant switches from mysqldump command
    2. Clarified what was happening during the running of the external
       global-sql-update and ~/sql-update files.
    3. Changed some wording for echo statements.

--- sql-update.sh
+++ sql-update.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+# There should never be a reason to run this script as root, so disabling it
+if [[ ${EUID} -eq 0 ]]; then
+  echo "This script should be run with your normal user account."
+  exit 1
+fi
+
+# Fully Qualified domain name of this server
 FQDN=$(hostname -f)
+
+# The name of the destination database, based on hostname
 DB=$(hostname | sed 's/-/_/g')
+
+# The name of the source database, extraced from hostname
 MASTERDB=$(hostname | sed 's/\(.-\).*/\1/; s/-//g;')
+
+# A Read Only account that has SELECT permissions on the "source" database
 MASTERDBUSER="ro_account"
 MASTERDBPASS="yoursecretpassword"
+
+# Read Only DB server (typically would be a slave to the production DB server)
 RODBSERVER="dbro.yourdomain.com"
+
+# Your web servers DocRoot directory
 WEBROOT="/var/www"
 
 echo "Refreshing ${DB} database..."
@@ -19,10 +36,10 @@ drush sql-drop -y
 
 # Copy db from prod.
 echo "Copying ${MASTERDB} database to ${DB}..."
-mysqldump -v -q --add-drop-table --add-locks --complete-insert --create-options --disable-keys --lock-tables --quick --set-charset --hex-blob --max-allowed-packet=1073741824 --single-transaction -u ${MASTERDBUSER} -h ${RODBSERVER} -p${MASTERDBPASS} ${MASTERDB} --single-transaction --ssl-key=/etc/ssl/mariadb/${FQDN}.key --ssl-cert=/etc/ssl/mariadb/${FQDN}.crt --ssl-ca=/etc/ssl/mariadb/ca.crt | drush sql-cli
+mysqldump -v --opt --complete-insert --hex-blob --max-allowed-packet=1073741824 --single-transaction -u ${MASTERDBUSER} -h ${RODBSERVER} -p${MASTERDBPASS} ${MASTERDB} --ssl-key=/etc/ssl/mariadb/${FQDN}.key --ssl-cert=/etc/ssl/mariadb/${FQDN}.crt --ssl-ca=/etc/ssl/mariadb/ca.crt | drush sql-cli
 
 # Truncate the cache tables.
-echo "Truncating database cache tables since we use memcached..."
+echo "Truncating database cache tables..."
 IFSSAVE=${IFS}
 IFS=$'\n'
 
@@ -36,26 +53,29 @@ done
 IFS=${IFSSAVE}
 
 # If memcached is running, flush it.
-if [[ `sudo /etc/init.d/memcached status` =~ 'memcached is running' ]]; then
+if [[ `sudo service memcached status` =~ 'memcached is running' ]]; then
   echo "flush_all" | nc localhost 11211
   echo "memcached flushed!"
 fi
 
 # Clear Drupal caches.
+echo "Clearing Drupal caches"
 drush cc all
 
 # run global sql-update command file
 if [[ -s /usr/local/bin/global-sql-update ]]; then
-  echo "Running Global sql-update commands"
+  echo -n "Running Global sql-update commands . . . "
   . /usr/local/bin/global-sql-update
+  echo "Complete"
 else
   echo "Global sql-update command file not found"
 fi
 
 # Run user sql-update command file
 if [[ -s ~/sql-update ]]; then
-  echo "Running $(whoami)'s sql-update commands"
+  echo -n "Running $(whoami)'s sql-update commands . . . "
   . ~/sql-update
+  echo "Complete
 else
   echo "No sql-update command file found for $(whoami)"
 fi
